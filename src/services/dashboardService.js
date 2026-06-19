f29da132c7a70bcef762f7285cf89cb1745fe33e
Le Hong Linh <linhlh@rever.vn>
2026-06-19 11:10:13 +0700

    fix login

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,14 +1,7 @@
 // services/dashboardService.js
 import { apiClient } from "@/services/api";
 
-// GET /api/dashboard
-// BE trả về: { summary: {...}, categoryStats: [...], departmentStats: [...], statusStats: [...] }
-// export const getDashboardStats = async () => {
-//   const data = await apiClient.get("/dashboard");
-//   return data; // ✅ apiClient.get() đã trả về dữ liệu trực tiếp
-// };
-
 export const getDashboardStats = async () => {
-  const response = await apiClient.get("/api/dashboard");
+  const response = await apiClient.get("/dashboard");
   return response; // ✅ không cần .data
 };
